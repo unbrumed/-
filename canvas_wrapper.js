0edfc62374cbd9f845682766dfd07b03146b2bac
Mathy <tynroar@gmail.com>
2023-05-24 06:55:34 +0300

    format

--- canvas_wrapper.js
+++ canvas_wrapper.js
@@ -1,66 +1,67 @@
 function contructor() {
   this.refsize_el = null;
   this.canvas = null;
-	this.context = null;
-	this.width_units = 1;
+  this.context = null;
+  this.width_units = 1;
 
-	return this;
+  return this;
 }
 
 export default function UnbrumedCanvas() {
-	constructor.apply(this);
-	this.pxsize = {
-		width: 0,
-		height: 0
-	}
+  constructor.apply(this);
+  this.pxsize = {
+    width: 0,
+    height: 0,
+  };
 
-	/**
-	 * @param {HTMLElement} canvas
+  /**
+   * @param {HTMLElement} canvas
    * @param {HTMLElement|window} refwidth/height reference element
-	 * @param {number} units draw height in unis
-	 */
+   * @param {number} units draw height in unis
+   */
   this.init = (canvas, ref, units) => {
     this.canvas = canvas;
-		this.width_units = units;
-		this.context = canvas.getContext("2d");
+    this.width_units = units;
+    this.context = canvas.getContext("2d");
     this.refsize_el = reference;
   };
 
-	/**
-	 * units to pixels
-	 *
-	 * @returns {number} canvas absolute units converted into pixels
-	 */
-	this.utopx = (units) => {
-		return Math.floor(this.unitsize() * units);
-	}
+  /**
+   * units to pixels
+   *
+   * @returns {number} canvas absolute units converted into pixels
+   */
+  this.utopx = (units) => {
+    return Math.floor(this.unitsize() * units);
+  };
 
-	this.pxtou = (pixels) => {
-		return pixels / this.unitsize();
-	}
+  this.pxtou = (pixels) => {
+    return pixels / this.unitsize();
+  };
 
-	this.unitsize = () => {
-		return this.pxsize.width / this.width_units;
-	}
+  this.unitsize = () => {
+    return this.pxsize.width / this.width_units;
+  };
 
-	this.uwidth = () => {
-		return this.width_units;
-	}
+  this.uwidth = () => {
+    return this.width_units;
+  };
 
-	this.uheight = () => {
-		return this.pxtou(this.pxsize.height);
-	}
+  this.uheight = () => {
+    return this.pxtou(this.pxsize.height);
+  };
 
   this.update = () => {
     if (this.refsize_el) {
       const width = this.refsize_el.clientWidth ?? this.refsize_el.innerWidth;
-      const height = this.refsize_el.clientHeight ?? this.refsize_el.innerHeight;
+      const height =
+        this.refsize_el.clientHeight ?? this.refsize_el.innerHeight;
 
-			this.pxsize.width = width;
-			this.pxsize.height = height;
+      this.pxsize.width = width;
+      this.pxsize.height = height;
 
-			this.canvas.width = width;
-			this.canvas.height = height;
+      this.canvas.width = width;
+      this.canvas.height = height;
     }
   };
 }
